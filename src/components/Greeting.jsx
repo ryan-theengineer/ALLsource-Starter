7680dec8cc37c338c42363e0536a9e87d675ad5b
Ryan Stanton <ryanstanton9123@gmail.com>
2023-07-30 12:50:23 -0700

    updated css file with the following properties: .greeting, .blog-links, dark mode and light mode

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -9,7 +9,7 @@ export default function Greeting({ messages }) {
 
   return (
     <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
+      <h3 class="greeting">{greeting}! Thank you for visiting!</h3>
       <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
     </div>
   );
